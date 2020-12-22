510303f5a46bb094c560f69a3ff31acb102058b6
Ponglert <71001516+Ponglert@users.noreply.github.com>
2020-12-23 06:47:55 +0700

    Update asteriskprint.java

--- asteriskprint.java
+++ asteriskprint.java
@@ -8,18 +8,18 @@ public class asteriskprint {
 		// TODO Auto-generated method stub
 		int n;
 		Scanner reader = new Scanner(System.in);
-        System.out.print("Enter int rows : ");
-        n = reader.nextInt();
-        for(int i = 0; i < n; i++) {
-        	for(int j = 0; j < n; j++) {
-        		if(j >= n-i-1) {
-        			System.out.print("*");
-        		}else {
-        			System.out.print(" ");
-        		}
-        	}
-        	System.out.println();
-        }
+		System.out.print("Enter int rows : ");
+		n = reader.nextInt();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(j >= n-i-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
