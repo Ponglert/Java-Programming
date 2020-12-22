c1bb2f9bb732cc5a3dd19f876c1f56fd90f04e62
Ponglert <71001516+Ponglert@users.noreply.github.com>
2020-12-23 06:40:33 +0700

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,34 @@
+package testch5_3;
+
+import java.util.Scanner;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a,b,y;
+		char oper;
+		Scanner reader = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        a = reader.nextFloat();
+        System.out.print("Enter second number: ");
+        b = reader.nextFloat();
+        System.out.print("Enter operation: ");
+        oper = reader.next().charAt(0);
+        
+        y = 0;
+        if(oper == '+'){
+        	y = a +b;
+        }else if(oper == '-') {
+        	y = a - b;
+        }else if(oper == '*') {
+        	y = a * b;
+        }else if(oper == '/') {
+        	y = a / b;
+        }else if(oper == '%') {
+        	y = a % b;
+        }
+        System.out.println("y = " + y);
+	}
+
+}
